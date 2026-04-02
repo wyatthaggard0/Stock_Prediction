d682479d1e6be7eb372f7d56483f2d0c5dff303b
wyatthaggard0 <wyatthaggard0@gmail.com>
2026-04-02 16:53:03 -0500

    Update StreamlitApp_HW5_Part_1.py

--- Portfolio/StreamlitApp_HW5_Part_1.py
+++ Portfolio/StreamlitApp_HW5_Part_1.py
@@ -30,7 +30,7 @@ project_root = os.path.abspath(os.path.join(current_dir, '..'))
 if project_root not in sys.path:
     sys.path.append(project_root)
 
-from src.feature_utils import extract_features
+#from src.feature_utils import extract_features
 
 # Access the secrets
 aws_id = st.secrets["aws_credentials"]["AWS_ACCESS_KEY_ID"]
@@ -53,14 +53,14 @@ session = get_session(aws_id, aws_secret, aws_token)
 sm_session = sagemaker.Session(boto_session=session)
 
 # Data & Model Configuration
-df_features = extract_features()
+#df_features = extract_features()
 
 MODEL_INFO = {
         "endpoint": aws_endpoint,
-        "explainer": 'explainer.shap',
-        "pipeline": 'finalized_model.tar.gz',
-        "keys": ["RIVN", "TSLA", "LCID", "NIO" "DEXJPUS", "DEXUSUK", "SP500", "DJIA", "VIXCLS"],
-        "inputs": [{"name": k, "type": "number", "min": -1.0, "max": 1.0, "default": 0.0, "step": 0.01} for k in ["RIVN", "TSLA","LCID", "NIO" "DEXJPUS", "DEXUSUK", "SP500", "DJIA", "VIXCLS"]]
+        "explainer": 'explainer_pca.shap',
+        "pipeline": 'finalized_pca_model.tar.gz',
+        "keys": ["IBM"],
+        "inputs": [{"name": k, "type": "number", "min": 0.0, "default": 100.0, "step": 10.0} for k in ["IBM"]]
 }
 
 def load_pipeline(_session, bucket, key):
@@ -112,6 +112,28 @@ def display_explanation(input_df, session, aws_bucket):
     explainer_name = MODEL_INFO["explainer"]
     explainer = load_shap_explainer(session, aws_bucket, posixpath.join('explainer', explainer_name),os.path.join(tempfile.gettempdir(), explainer_name))
     shap_values = explainer(input_df)
+
+    dataset = pd.read_csv(r'./SP500Data.csv',index_col=0)
+    random = 'IBM'
+    random_price = json.loads(request_body)[random]
+    closest_date = (dataset[random] - float(random_price)).abs().idxmin()
+
+    return_period=5
+
+    X = np.log(dataset.drop([random],axis=1)).diff(return_period)
+    X = np.exp(X).cumsum()
+    X.columns = [name + "_CR_Cum" for name in X.columns]
+
+    input_df=X.loc[[closest_date]]
+
+    best_pipeline = load_pipeline(session, aws_bucket, 'sklearn-pipeline-deployment')
+
+    preprocessing_pipeline = Pipeline(steps=best_pipeline.steps[0:2])
+    input_df_transformed = preprocessing_pipeline.transform(X)
+    feature_names = best_pipeline[:-1].get_feature_names_out()
+    input_df_transformed = pd.DataFrame(input_df_transformed, columns=feature_names)
+    shap_values = explainer(input_df_transformed)
+
     st.subheader("🔍 Decision Transparency (SHAP)")
     fig, ax = plt.subplots(figsize=(10, 4))
     shap.plots.waterfall(shap_values[0], max_display=10)
@@ -133,22 +155,22 @@ with st.form("pred_form"):
         with cols[i % 2]:
             user_inputs[inp['name']] = st.number_input(
                 inp['name'].replace('_', ' ').upper(),
-                min_value=inp['min'], max_value=inp['max'], value=inp['default'], step=inp['step']
+                min_value=inp['min'], value=inp['default'], step=inp['step']
             )
     
     submitted = st.form_submit_button("Run Prediction")
 
 if submitted:
 
-    data_row = [user_inputs[k] for k in MODEL_INFO["keys"]]
+    #data_row = [user_inputs[k] for k in MODEL_INFO["keys"]]
     # Prepare data
-    base_df = df_features
-    input_df = pd.concat([base_df, pd.DataFrame([data_row], columns=base_df.columns)])
+    #base_df = df_features
+    #input_df = pd.concat([base_df, pd.DataFrame([data_row], columns=base_df.columns)])
     
-    res, status = call_model_api(input_df)
+    res, status = call_model_api(user_inputs)
     if status == 200:
         st.metric("Prediction Result", res)
-        display_explanation(input_df,session, aws_bucket)
+        display_explanation(user_inputs,session, aws_bucket)
     else:
         st.error(res)
 
